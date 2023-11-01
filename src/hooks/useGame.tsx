a25aa553a1924bfd00ac715683b3d36c8e826c4c
Julian Barbosa <julianbarbossa@hotmail.com>
2023-10-31 21:50:29 -0500

    create new funcion to filter games

--- src/hooks/useGame.tsx
+++ src/hooks/useGame.tsx
@@ -4,13 +4,18 @@ import axios from "axios";
 
 const useGame = () => {
   const [game, setGame] = useState<GameInterface>();
+  const [ allGames, setAllGames ] = useState<GameInterface[]>([])
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [error, setError] = useState<string>("");
+  const [ gameByCity, setGameByCity ] = useState<GameInterface[]>()
+  const [ gameBySport, setGameBySport ] = useState<GameInterface[]>()
 
+
+  const URL = "https://j2vk6ieugf.execute-api.us-east-1.amazonaws.com/prod/game"
   const createGame = async (gameData: GameInterface) => {
     try {
       setIsLoading(true);
-      const response = await axios.post("https://j2vk6ieugf.execute-api.us-east-1.amazonaws.com/prod/game", gameData);
+      const response = await axios.post(URL, gameData);
       if (response.status === 201) {
         setGame(response.data);
       } else {
@@ -23,7 +28,55 @@ const useGame = () => {
     }
   };
 
-  return { createGame, game, isLoading, error };
+  const getAllGames = async () => {
+    try {
+      setIsLoading(true);
+      const response = await axios.get(URL);
+      if (response.status === 200) {
+        setAllGames(response.data);
+      } else {
+        setError("Error getting game");
+      }
+    } catch (error) {
+      console.log(error)
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  const getGameByCity = async (cityId: number) => { 
+    try {
+      setIsLoading(true);
+      const response = await axios.get(`${URL}/${cityId}`);
+      if (response.status === 200) {
+        setGameByCity(response.data);
+      } else {
+        setError("Error getting game");
+      }
+    } catch (error) {
+      console.log(error)
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  const getGameBySport = async (sportId: number) => {
+    try {
+      setIsLoading(true);
+      const response = await axios.get(`${URL}/sports/${sportId}`);
+      if ( response.status === 200) {
+        setGameBySport(response.data);
+      } else {
+        setError("Error getting game");
+      }
+    } catch (error) {
+      console.log(error)
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  return { createGame, game, isLoading, error ,getGameByCity, gameByCity, getGameBySport, gameBySport, getAllGames, allGames };
 };
 
 export default useGame;
