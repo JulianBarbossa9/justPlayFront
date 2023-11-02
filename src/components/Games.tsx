4520c757dd7d53cc0970f5faf870cf80f11f7bc8
Julian Barbosa <julianbarbossa@hotmail.com>
2023-11-01 19:40:31 -0500

    add format and add param delete

--- src/components/Games.tsx
+++ src/components/Games.tsx
@@ -1,83 +1,93 @@
-import React, { useEffect, useState } from 'react'
-import GameCard from './GameCard'
-import { GameInterface } from '../interface/game.interface'
-import useGame from '../hooks/useGame'
-import { MdOutlineDelete} from 'react-icons/md'
+import React, { useEffect, useState } from "react";
+import GameCard from "./GameCard";
+import { GameInterface } from "../interface/game.interface";
+import { MdOutlineDelete } from "react-icons/md";
 
 interface GamesProps {
-  cityFilter?: number
-  sportFilter?: number,
-  handleSearch?: (searchTerm: React.ChangeEvent<HTMLInputElement>) => void,
-  search?: string
-  allGames: GameInterface[]
-  getAllGames: () => void
-  getGameByCity: (cityId: number) => void
-  getGameBySport: (sportId: number) => void
+  cityFilter?: number;
+  sportFilter?: number;
+  handleSearch?: (searchTerm: React.ChangeEvent<HTMLInputElement>) => void;
+  search?: string;
+  allGames: GameInterface[];
+  getAllGames: () => void;
+  getGameByCity: (cityId: number) => void;
+  getGameBySport: (sportId: number) => void;
+  deleteGame: (gameId: number) => void;
 }
 
-const Games = ({ cityFilter, sportFilter, search, allGames, getAllGames ,getGameByCity, getGameBySport } : GamesProps) => {
-
-  const [ games, setGames ] = useState<GameInterface[]>([])
+const Games = ({
+  cityFilter,
+  sportFilter,
+  search,
+  allGames,
+  getAllGames,
+  getGameByCity,
+  getGameBySport,
+  deleteGame
+}: GamesProps) => {
+  const [games, setGames] = useState<GameInterface[]>([]);
   
-  
-  const resultSearch = allGames?.filter((data) =>
-    data.name.toLowerCase().includes(search?.toLowerCase() ?? '')
-  ) ?? []
 
-
-
+  const resultSearch =
+  (allGames ? allGames : []).filter((data) =>
+    data.name.toLowerCase().includes(search?.toLowerCase() ?? "")
+  );
 
   const fetchGames = async () => {
-    if(cityFilter){
-      getGameByCity(cityFilter)
-    } else if(sportFilter){
-      getGameBySport(sportFilter)
+    if (cityFilter) {
+      getGameByCity(cityFilter);
+    } else if (sportFilter) {
+      getGameBySport(sportFilter);
     }
-  }
+  };
 
   useEffect(() => {
-    if(search){
-      setGames(resultSearch)
+    if (search) {
+      setGames(resultSearch);
     } else {
-      setGames(allGames )
+      setGames(allGames);
     }
-  }, [search, allGames])
+  }, [search, allGames]);
 
   useEffect(() => {
-    fetchGames()
-    getAllGames()
-  }, [cityFilter, sportFilter])
+    fetchGames();
+    getAllGames();
+  }, [cityFilter, sportFilter]);
 
   const renderGames = search ? resultSearch : games;
 
-  useEffect(() =>{
-    setGames(allGames )
-  },[allGames])
-  
+  useEffect(() => {
+    setGames(allGames);
+  }, [allGames]);
+
   return (
     <>
-    <main>
-        <div className='grid gap-16 grid-cols-fluid' >
+      <main>
+        <div className="grid gap-16 grid-cols-fluid">
           {renderGames.length > 0 ? (
             renderGames.map((game: any) => {
               return (
                 <>
-                <div className='flex flex-col' key={game.id}>
-                
-                <GameCard
-                  name={game.name}
-                  city={game.city}
-                  endTime={game.endTime}
-                  startTime={game.startTime}
-                  sport={game.sport}
-                  createdAt={game.createdAt}
-                  description={game.description}
-                  updatedAt={game.updatedAt}
-                  key={game.id}
-                />
-               
-                </div>
-                
+                  <div key={game.id}>
+                    <GameCard
+                      name={game.name}
+                      city={game.city}
+                      endTime={game.endTime}
+                      startTime={game.startTime}
+                      sport={game.sport}
+                      createdAt={game.createdAt}
+                      description={game.description}
+                      updatedAt={game.updatedAt}
+                      key={game.id}
+                    />
+                    <div className="flex">
+                      <button
+                        onClick={() => deleteGame(game.id)} 
+                        className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-full ml-5 mt-2">
+                        <MdOutlineDelete />
+                      </button>
+                    </div>
+                  </div>
                 </>
               );
             })
@@ -87,7 +97,7 @@ const Games = ({ cityFilter, sportFilter, search, allGames, getAllGames ,getGame
         </div>
       </main>
     </>
-  )
-}
+  );
+};
 
-export default Games
+export default Games;
