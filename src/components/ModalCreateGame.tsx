962b9733e0c74a6a3aa065dfe409b22475eaa437
Julian Barbosa <julianbarbossa@hotmail.com>
2023-11-01 21:51:35 -0500

    update title and button when is updated

--- src/components/ModalCreateGame.tsx
+++ src/components/ModalCreateGame.tsx
@@ -83,7 +83,7 @@ const ModalCreateGame: React.FC<ModalCreateGameProps> = ({ onClose, createGame, 
       { error && <Error>Fields with <span>*</span> are Required </Error>}
         <form onSubmit={(event) => handleSubmit(event)}>
           <div className='border-b border-gray-900/10 pb-12 mt-10'>
-            <h2 className="text-2xl font-bold   leading-7 text-gray-900">Create Game</h2>
+            <h2 className="text-2xl font-bold   leading-7 text-gray-900">{selectedGame ? 'Edit Game' : "Create a Game"}</h2>
             <p className="mt-1 text-sm leading-6 text-gray-600">Create a game to play with some friends.</p>
             
             <div className='mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6'>
@@ -98,7 +98,8 @@ const ModalCreateGame: React.FC<ModalCreateGameProps> = ({ onClose, createGame, 
                     onChange={(e) => setName(e.target.value)} 
                     id='name-game'
                     autoComplete='given-name'
-                    className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
+                    className='block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
+            
                   />
                 </div>
               </div>
@@ -115,7 +116,7 @@ const ModalCreateGame: React.FC<ModalCreateGameProps> = ({ onClose, createGame, 
                     value={description} 
                     onChange={(e) => setDescription(e.target.value)} 
                     id='description-game'
-                    className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
+                    className='block w-full px-2 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6'
                   />
                 </div>
               </div>
@@ -133,7 +134,7 @@ const ModalCreateGame: React.FC<ModalCreateGameProps> = ({ onClose, createGame, 
                     value={startTime instanceof Date ? startTime.toISOString() : startTime} 
                     onChange={(e) => setStartTime(e.target.value)}
                     id='start-game'
-                    className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
+                    className='block w-full px-2 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
                   />
                 </div>
               </div>
@@ -151,7 +152,7 @@ const ModalCreateGame: React.FC<ModalCreateGameProps> = ({ onClose, createGame, 
                     value={endTime instanceof Date ? endTime.toISOString() : endTime} 
                     onChange={(e) => setEndTime(e.target.value)} 
                     id='end-game'
-                    className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
+                    className='block w-full px-2 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
                   />
                 </div>
               </div>
@@ -215,7 +216,7 @@ const ModalCreateGame: React.FC<ModalCreateGameProps> = ({ onClose, createGame, 
             </div>
           
           </div>
-          <button type="submit" className='py-2 px-8  rounded-xl font-bold text-slate-100 bg-mainlyButton mt-5'>Create</button>
+          <button type="submit" className='py-2 px-8  rounded-xl font-bold text-slate-100 bg-mainlyButton mt-5'>{ selectedGame ? "Edit Game" : "Create Game"}</button>
           
         </form>
         <button className='mt-5 border-2 rounded-md p-4' onClick={onClose}>
