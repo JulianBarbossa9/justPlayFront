73b4c85421f260e39ebe1b416fce8e0394df5d56
Julian Barbosa <julianbarbossa@hotmail.com>
2023-11-01 19:11:59 -0500

    edit filters and add new params

--- src/components/Games.tsx
+++ src/components/Games.tsx
@@ -2,46 +2,36 @@ import React, { useEffect, useState } from 'react'
 import GameCard from './GameCard'
 import { GameInterface } from '../interface/game.interface'
 import useGame from '../hooks/useGame'
+import { MdOutlineDelete} from 'react-icons/md'
 
 interface GamesProps {
   cityFilter?: number
   sportFilter?: number,
   handleSearch?: (searchTerm: React.ChangeEvent<HTMLInputElement>) => void,
   search?: string
+  allGames: GameInterface[]
+  getAllGames: () => void
+  getGameByCity: (cityId: number) => void
+  getGameBySport: (sportId: number) => void
 }
 
-const Games = ({ cityFilter, sportFilter, handleSearch, search} : GamesProps) => {
+const Games = ({ cityFilter, sportFilter, search, allGames, getAllGames ,getGameByCity, getGameBySport } : GamesProps) => {
 
   const [ games, setGames ] = useState<GameInterface[]>([])
   
-  const { allGames, getAllGames } = useGame()
   
-  const resultSearch = allGames.filter((data) =>
+  const resultSearch = allGames?.filter((data) =>
     data.name.toLowerCase().includes(search?.toLowerCase() ?? '')
-  )
+  ) ?? []
+
+
 
 
   const fetchGames = async () => {
-    let url = "https://j2vk6ieugf.execute-api.us-east-1.amazonaws.com/prod/game"
-    try {
-      if(cityFilter && sportFilter){
-        url
-      } else if( cityFilter ){
-        url += `/${cityFilter}`
-      }else if ( sportFilter ){
-        url += `/sports/${sportFilter}`
-      }
-      if (search) {
-        url += `?search=${search}`
-      }
-
-    
-
-      const searhResponse = await fetch(url)
-      const searchResult = await searhResponse.json()
-      setGames(searchResult)
-    } catch (error) {
-      console.log(error)
+    if(cityFilter){
+      getGameByCity(cityFilter)
+    } else if(sportFilter){
+      getGameBySport(sportFilter)
     }
   }
 
@@ -49,7 +39,7 @@ const Games = ({ cityFilter, sportFilter, handleSearch, search} : GamesProps) =>
     if(search){
       setGames(resultSearch)
     } else {
-      setGames(allGames)
+      setGames(allGames )
     }
   }, [search, allGames])
 
@@ -59,14 +49,21 @@ const Games = ({ cityFilter, sportFilter, handleSearch, search} : GamesProps) =>
   }, [cityFilter, sportFilter])
 
   const renderGames = search ? resultSearch : games;
+
+  useEffect(() =>{
+    setGames(allGames )
+  },[allGames])
   
   return (
     <>
     <main>
-        <div className='grid gap-16 grid-cols-fluid'>
+        <div className='grid gap-16 grid-cols-fluid' >
           {renderGames.length > 0 ? (
             renderGames.map((game: any) => {
               return (
+                <>
+                <div className='flex flex-col' key={game.id}>
+                
                 <GameCard
                   name={game.name}
                   city={game.city}
@@ -78,6 +75,10 @@ const Games = ({ cityFilter, sportFilter, handleSearch, search} : GamesProps) =>
                   updatedAt={game.updatedAt}
                   key={game.id}
                 />
+               
+                </div>
+                
+                </>
               );
             })
           ) : (
